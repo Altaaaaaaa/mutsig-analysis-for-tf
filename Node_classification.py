ff0109592e003f0681775453cc0fd8cc322c1633
Yooeun Kim <79960865+ys910111@users.noreply.github.com>
2024-07-20 17:18:03 +0900

    Update Node_classification.py

--- Node_classification.py
+++ Node_classification.py
@@ -4,45 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 
-# 인자값을 받을 수 있는 인스턴스 생성
 parser = argparse.ArgumentParser(description='de novo-cosmic cosine similarity')
 
-# 입력받을 인자값 등록
+parser.add_argument('--corr_dir', required=True, help='Directory of correlation results')
 parser.add_argument('--pos_neg', required=True, help='select pos/neg group(pos or neg)')
-parser.add_argument('--tf_group_file', required=True, help='tf-tg data divided into two groups')
+parser.add_argument('--gsva', required=True, help='file of GSVA results')
 parser.add_argument('--sig_num', required=True, help='enter the number of signature')
+parser.add_argument('--out_dir', required=True, help='Directory of node classification results')
 
-
-# 입력받은 인자값을 args에 저장 (type: namespace)
 args = parser.parse_args()
 
+cor_output = pd.read_csv(f"{args.corr_dir}/Filt_Result_"+str(args.pos_neg)+".csv", sep=',', index_col=0)
+TF_list=list(set(cor_output.gene))
 
-
-
-
-
-
-
-
-
-
-cor_output = pd.read_csv("./output/Cor/Filt_Result_"+str(args.pos_neg)+".csv", sep=',', index_col=0)
-TF_list=list(set(cor_output.gene)) # TF의 symbol id 리스트로 만들어주기(중복제거)
-
-
-##########바꿔주기
-# TF데이터(GSVA 전, group별로 분리)
-gsva_cor = pd.read_csv(str(args.tf_file), sep=',', index_col=0)
-
-
-
-
-
-
-
-
-
-
+gsva_cor = pd.read_csv(str(args.gsva), sep=',', index_col=0)
+os.makedirs(args.out_dir, exist_ok=True)
 
 def make_edge(pn):
     tg_group = gsva_cor[gsva_cor['TF'].str.contains('_'+str(pn), na = False)]
@@ -87,7 +63,7 @@ else:
 
 
 
-# signature를 0,1로 표현한 데이터프레임 만들기
+# Create a dataframe that represents signatures as 0s and 1s
 gene_sig_df = pd.DataFrame(index=TF_list,columns=['sig.'+str(i) for i in range(int(args.sig_num))])
 gene_sig_df = gene_sig_df.fillna(0)
 
@@ -124,7 +100,7 @@ def node_iter(n,node_info,mul_gene):
 
     node_gene_sig = node_info
 
-    # 1번째로 TG들의 node label 설정
+    # First, set node labels for TGs
 
     #print(f"iter: {1}")
 
@@ -132,24 +108,24 @@ def node_iter(n,node_info,mul_gene):
         count = node_gene_sig[i]
         prot1 = gene_sig_df.index[i]
         
-        if i >= 5: # tg들
+        if i >= 5: 
             prot2 = link_filt[link_filt['protein2'] == prot1]
-            #print(prot2) # 해당 gene과 연결된 edge 정보 출력
+            #print(prot2)
 
             for k in range(len(prot2)):
                 node_gene = prot2.iloc[k][0]
                 count+=prot2.iloc[k][2] * np.array(gene_sig_df.loc[node_gene])
                 count = count.tolist()
-            #print(f'count : {count}') # edge 정보(가중치) x label 결과
+            #print(f'count : {count}')
 
-        idx = np.array(np.nonzero(count)).tolist() # count 배열 중 큰 label을 index로 설정하여 출력
+        idx = np.array(np.nonzero(count)).tolist() 
         re_count = [0 for i in range(9)]
         for nz in idx[0]:
             re_count[nz] = 1
         node_gene_sig[i] = re_count
 
 
-    # 두번째부터는 edge 정보를 이용해서
+    # Second, use edge information
     for it in range(1,n):
         #print(f"iter: {it+1}")
 
@@ -161,22 +137,22 @@ def node_iter(n,node_info,mul_gene):
                 if it == 1:
                     mul_gene.append(prot1)
                 
-                if i < 5: # TF들
+                if i < 5: # TFs
                     prot2 = link_filt[link_filt['protein1'] == prot1]
-                    #print(prot2) # 해당 gene과 연결된 edge 정보 출력
+                    #print(prot2) 
 
                     for k in range(len(prot2)):
                         node_gene = prot2.iloc[k][1]
                         count+=prot2.iloc[k][2] * np.array(gene_sig_df.loc[node_gene])
                         count = count.tolist()
-                    #print(f'count : {count}') # edge 정보(가중치) x label 결과
-                elif i >= 5: # tg들
+                    #print(f'count : {count}') 
+                elif i >= 5: # TGs
                     prot2 = link_filt[link_filt['protein2'] == prot1]
                     for k in range(len(prot2)):
                         node_gene = prot2.iloc[k][0]
                         count+=prot2.iloc[k][2] * np.array(gene_sig_df.loc[node_gene])
                         count = count.tolist()
-                    #print(f'count : {count}') # edge 정보(가중치) x label 결과
+                    #print(f'count : {count}') 
             
                 idx = np.argmax(count) # count 배열 중 큰 label을 index로 설정하여 출력
                 re_count = [0 for i in range(9)]
@@ -211,7 +187,7 @@ df_columns = ['sig.'+str(i) for i in range(int(args.sig_num))]
 
 gene_sig_df2 = pd.DataFrame(data=node_gene_sig, index=df_index, columns=df_columns, dtype=None, copy=False)
 
-gene_sig_df2.to_csv('./output/Node_classi/node_result_'+str(args.pos_neg)+".csv", sep=',', index=True, header=True)
+gene_sig_df2.to_csv(f'{args.out_dir}/node_result_'+str(args.pos_neg)+".csv", sep=',', index=True, header=True)
 
 
 
@@ -221,7 +197,7 @@ gene_sig_df2.to_csv('./output/Node_classi/node_result_'+str(args.pos_neg)+".csv"
 
 
 
-# 그래프를 그리기 위한 node 리스트 구하기
+# Obtain a list of nodes for plotting a graph
 
 list_sig = []
 
@@ -251,23 +227,21 @@ link_filt['combined_score'] = link_filt['combined_score'].transform(lambda x : 1
 
 
 
-# 그래프 생성
 classi_G = nx.from_pandas_edgelist(link_filt, 'protein1', 'protein2', edge_attr='combined_score', create_using = nx.Graph())
 
 
-# 컬러 리스트
 c_list = ['skyblue', 'pink', 'Magenta', 'Yellow', 'Gray', 'Purple', 'Brown', '#cbbbf4', 'Teal']
 
 c_list2 = ['#5CACEE', '#FF6B8B', 'Magenta', 'Yellow', 'Gray', 'Purple', 'Brown', '#6D66C2', 'Teal']
 
-# 그래프 그리기
+
 pos = nx.spring_layout(classi_G)
 
-# 노드 리스트별로 그래프에 노드 추가
+
 for i, node_list in enumerate(list_sig):
     nx.draw_networkx_nodes(classi_G, pos, nodelist=node_list, node_color=c_list[i], label=f'{col[i]}', node_size=50)
     
-    # TF의 노드 그리기,레이블 표시
+
 for i, tf_list in enumerate(tf_sig):
     nx.draw_networkx_nodes(classi_G, pos, nodelist=tf_list, node_color=c_list[i], edgecolors=c_list2[i], linewidths=2, node_size=2500) 
 
@@ -283,7 +257,7 @@ nx.draw_networkx_labels(classi_G, pos, labels=node_labels, font_size=15, font_co
 
 
 
-plt.savefig('./output/Node_classi/node_figure_'+str(args.pos_neg)+'png')
+plt.savefig(f'{args.out_dir}/node_figure_'+str(args.pos_neg)+'.png')
 
 plt.rcParams['figure.figsize'] = [20, 10]
 plt.legend(loc='upper right', fontsize=15)
